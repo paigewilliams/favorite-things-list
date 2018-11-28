1c6b659adf3a278dddc3182142e855fe641c5c39
Kristin Brewer-Lowe and Paige Williams <paige@>
2018-11-28 11:08:38 -0800

    Ad methods for sort, delete, find, clear, exist

--- lib/project.rb
+++ lib/project.rb
@@ -5,6 +5,7 @@ class Item
 
   attr_accessor (:name)
   attr_accessor (:rank)
+  attr_accessor (:id)
 
   def initialize(name, rank)
     @name = name
@@ -12,13 +13,48 @@ class Item
     @id = @@counter
   end
 
-  def self.all
-  @@list
-  end
-
   def add(name, rank)
-
     @@counter += 1
     @@list.push([name, rank, @id])
   end
+
+  def self.all
+    @@list
+  end
+
+  def self.clear()
+    @@list = []
+    @@counter = 0
+  end
+
+  def self.find(id)
+    @@list.each do |item|
+      if item[2] == id
+        return item
+      end
+    end
+  end
+
+  def self.delete(id)
+    @@list = @@list.delete_if {|item| item[2]==id}
+  end
+
+  def self.sort_by_rank
+    @@list.sort_by! {|item| item[1] }
+  end
+
+  def self.sort_by_id
+    @@list.sort_by! {|item| item[2] }
+  end
+
+  def self.exist?(name)
+    @@list.each do |item|
+      if item[0] == name
+        return true
+      else
+        return false
+      end
+    end
+  end
+
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -3,35 +3,88 @@ require('rspec')
 require('project')
 
 describe("Item")do
+  before() do
+    Item.clear()
+  end
+
   describe(".all")do
     it("ensures that the list is empty in the beginning")do
       expect(Item.all()).to(eq([]))
     end
   end
-  describe(".add")do
+  describe("#add")do
     it("saves items to list")do
     first_item = Item.new("bunnies", 1)
     first_item.add(first_item.name, first_item.rank)
     expect(Item.all()).to(eq([["bunnies", 1, 0]]))
     end
   end
-  # describe(".add")do
-  #   it("saves ranks to the list")do
-  #   item = Item.new("bunnies", 1)
-  #   expect(item.rank).to(eq(1))
-  #   end
-  # end
-  # describe(".add")do
-  #   it("saves ranks to the list")do
-  #   item = Item.new("bunnies", 1)
-  #   expect(item.name).to(eq("bunnies"))
-  #   end
-  # end
-  describe(".add")do
+  describe("#add")do
+    it("allows us to access the rank of each item")do
+    item = Item.new("bunnies", 1)
+    expect(item.rank).to(eq(1))
+    end
+  end
+  describe("#add")do
+    it("allows us to access the name of each item")do
+    item = Item.new("bunnies", 1)
+    expect(item.name).to(eq("bunnies"))
+    end
+  end
+  describe("#add")do
+    it("allows us to access the id of each item")do
+    item = Item.new("bunnies", 1)
+    expect(item.id).to(eq(0))
+    end
+  end
+  describe("#add")do
     it("creates a unique id for each item")do
-    first_item = Item.new("dogs", 2)
+    first_item = Item.new("bunnies", 1)
     first_item.add(first_item.name, first_item.rank)
+    second_item = Item.new("dogs", 2)
+    second_item.add(second_item.name, second_item.rank)
     expect(Item.all()).to(eq([["bunnies", 1, 0], ["dogs", 2, 1]]))
+    end
+  end
+  describe(".find")do
+    it("finds an item based off of the unique ID")do
+    first_item = Item.new("bunnies", 1)
+    first_item.add(first_item.name, first_item.rank)
+    expect(Item.find(0)).to(eq(["bunnies", 1, 0]))
+    end
+  end
+  describe(".delete")do
+    it("deletes an item based on the ID")do
+    first_item = Item.new("bunnies", 1)
+    first_item.add(first_item.name, first_item.rank)
+    second_item = Item.new("dogs", 2)
+    second_item.add(second_item.name, second_item.rank)
+    expect(Item.delete(0)).to(eq([["dogs", 2, 1]]))
+    end
+  end
+  describe(".sort_by_rank")do
+    it("sorts items based on the rank")do
+    first_item = Item.new("bunnies", 5)
+    first_item.add(first_item.name, first_item.rank)
+    second_item = Item.new("dogs", 2)
+    second_item.add(second_item.name, second_item.rank)
+    expect(Item.sort_by_rank).to(eq([["dogs", 2, 1], ["bunnies", 5, 0]]))
+    end
+  end
+  describe(".sort_by_id")do
+    it("sorts items based on ID")do
+    first_item = Item.new("bunnies", 5)
+    first_item.add(first_item.name, first_item.rank)
+    second_item = Item.new("dogs", 2)
+    second_item.add(second_item.name, second_item.rank)
+    expect(Item.sort_by_id).to(eq([["bunnies", 5, 0], ["dogs", 2, 1]]))
+    end
+  end
+  describe(".exist?")do
+    it("returns true if an item already exists")do
+    first_item = Item.new("bunnies", 5)
+    first_item.add(first_item.name, first_item.rank)
+    expect(Item.exist?("bunnies")).to(eq(true))
     end
   end
 
