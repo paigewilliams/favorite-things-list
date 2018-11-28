9726fb7a94b637b36d18a21799d358052c0fe549
Kristin Brewer-Lowe and Paige Williams <paige@>
2018-11-28 11:25:07 -0800

    Add UI to favorite things list.

--- app.rb
+++ app.rb
@@ -6,5 +6,14 @@ require('./lib/project')
 
 
 get('/')do
-  
+  erb(:input)
+end
+
+post('/output') do
+
+  name = params.fetch("name")
+  rank = params.fetch("rank")
+  @item = Item.new(name, rank)
+  @item.add(@item.name, @item.rank)
+  erb(:output)
 end
